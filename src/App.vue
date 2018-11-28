c45d1515b7e4548373d8658d4813da2d313591e2
ccdmndkut <37282688+ccdmndkut@users.noreply.github.com>
2018-11-28 13:04:31 -0500

    e

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <mainpage></mainpage>
+  <div id='app'>
+    <input type='text'>
   </div>
 </template>
 
@@ -23,4 +23,41 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: #ffffff;
+  padding: 25px;
+}
+input {
+  all: unset;
+  background-color: #f1f3f4;
+  height: 30px;
+  width: 30vw;
+  -moz-border-radius: 15px;
+  -webkit-border-radius: 15px;
+  border-radius: 15px;
+  padding: 15px;
+  outline: 0;
+  transition: 300ms;
+}
+input:hover {
+  background-color: #e7e9ec;
+}
+
+input:focus,
+textarea:focus,
+keygen:focus,
+select:focus {
+  all: unset;
+  height: 30px;
+  width: 30vw;
+  background-color: white;
+  border: 2px #ccdcfc solid;
+  -moz-border-radius: 15px;
+  -webkit-border-radius: 15px;
+  border-radius: 15px;
+  padding: 15px;
+  box-sizing: border-box;
+  outline: 0;
+  transition: 0s;
+}
 </style>
